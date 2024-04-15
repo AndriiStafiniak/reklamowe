3f4016cc93381712ed4361c09fd44c5a1be643a9
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-04-15 10:17:06 +0200

    added global styles and theme

---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    alto: "#FBFBFE",
+    mineShaft: "#252525",
+    white: "#FFFFFF",
+    littleGrey: "rgba(209, 213, 218, 0.30)",
+    scienceBlue: "#0366D6",
+    black: "#000000",
+    slateGray: "#6E7E91",
+    iron: "#D1D5DA",
+    semiDark: "rgba(54, 54, 54, 0.72)",
+    mercury: "#E5E5E5",
+    azureRadiance: "#0676F4",
+    semiGrey: "rgba(209, 213, 218, 0.30)",
+    anakiwa: "#A3CEFF",
+  },
+};
